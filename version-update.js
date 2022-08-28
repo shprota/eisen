bc37c5482438eebc8ab9425359f92b64add9d239
Constantine Poltyrev <shprota@gmail.com>
2022-08-28 16:17:18 +0300

    fix(ci): debug paths

--- version-update.js
+++ version-update.js
@@ -4,9 +4,11 @@ exports.preCommit = async (props) => {
     const chartPath = 'helm/publicapi/Chart.yml';
     fs.writeFileSync('version.txt', props.version);
     console.log('Wrote version.txt');
+/*
     const chart = fs.readFileSync(chartPath);
     fs.writeFileSync(chartPath, chart.toString()
         .replace(/^version: .*$/, `version: ${props.version}`)
         .replace(/^appVersion: .*$/, `appVersion: ${props.version}`));
     console.log('Wrote ', chartPath);
+*/
 }
