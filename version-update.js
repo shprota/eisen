fe8f6d1867cca388ca60441caa5bd75729b3ef78
Constantine Poltyrev <shprota@gmail.com>
2022-08-28 16:10:34 +0300

    fix(ci): debug pre-commit hook

--- version-update.js
+++ version-update.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 
 module.exports = function preCommit(props) {
     const chartPath = 'helm/publicapi/Chart.yml';
+    fs.writeFileSync('version.txt', props.version);
+    console.log('Wrote version.txt');
     const chart = fs.readFileSync(chartPath);
     fs.writeFileSync(chartPath, chart.toString()
         .replace(/^version: .*$/, `version: ${props.version}`)
         .replace(/^appVersion: .*$/, `appVersion: ${props.version}`));
+    console.log('Wrote ', chartPath);
 }
